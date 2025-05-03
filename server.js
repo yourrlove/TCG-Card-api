4b525243ccb1f5b0f0ce325626f06dce72016232
yourrlove <nanhvt2708@gmail.com>
2025-05-03 20:10:07 +0700

    feat: authentication: login & logout

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const app = require("./app");  
+const app = require("./src/app");  
 
 const PORT = process.env.SERVER_PORT || 3000
 
---
+++ src/routes/auth.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const AuthContoller = require('../controllers/auth.controller');
+const { asyncHandler } = require('../helpers/index');
+const { verifyToken } = require('../middlewares/auth');
+
+router.post('/signup', asyncHandler( AuthContoller.signUp ));
+router.post('/login', asyncHandler( AuthContoller.logIn ));
+router.get('/logout', asyncHandler( AuthContoller.logOut ));
+router.get('/getaccesstoken', asyncHandler( AuthContoller.handleRefreshToken ));
+
+module.exports = router;
