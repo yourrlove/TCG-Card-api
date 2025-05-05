315e47df48e3e87672ec4753098a480aada8893f
yourrlove <nanhvt2708@gmail.com>
2025-05-05 10:08:15 +0700

    feat: collections api implementation

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,6 +4,7 @@ const userController = require('../controllers/user.controller');
 const { verifyToken } = require('../middlewares/auth');
 const { asyncHandler } = require('../helpers/index');
 
+router.get('/', asyncHandler(userController.get_list_users));
 router.get('/me', verifyToken, asyncHandler(userController.get_current_user));
 
 module.exports = router;
